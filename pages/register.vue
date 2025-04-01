bf74cf5ca9cd6c7378e7c6bd2a6cad4e1b33689e
Ramo <ramo@Ramos-MacBook-Pro.local>
2025-04-01 22:11:22 +0700

    first commit

---
+++ pages/register.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex bg-black h-screen">
+    <!-- sidebar -->
+    <div class="bg-zinc-900 w-[516px] p-8">
+      <h1 class="flex"><i class="text-amber-500 ri-pencil-fill"></i> <p class="text-white">Note</p><p class="text-amber-500">Nest</p></h1>
+      <h1 class="text-white font-bold text-lg mt-8">Sign up for a free account</h1>
+      <p class="text-white text-sm mt-0.5">Already registered? <span class="text-amber-500 underline">log in</span> to your account</p>
+      <form class="mt-5"> 
+        <div class="flex flex-col">
+          <label class="text-white text-sm mb-1" for="">Email Address</label>
+          <input class="text-sm text-2 border-1" type="text" placeholder="Enter email address">
+        </div>
+      </form>
+    </div>
+    <!-- /sidebar -->
+    <!-- note intruduction -->
+    <div></div>
+    <!-- note intruduction -->
+
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
